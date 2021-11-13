8d02df63920c1bd5828ffa46646eacc432d99ad0
mqois16 <qoisoctava@gmail.com>
2021-11-13 16:19:16 +0700

    Monster Script

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
+    [HideInInspector]
+    public float speed;
+
+    private Rigidbody2D myBody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myBody = GetComponent<Rigidbody2D>();
+        speed = 7;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        myBody.velocity = new Vector2(speed, myBody.velocity.y);
     }
 }
