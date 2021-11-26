a93c6ec9a1861359c1cb21abade62fccb50aaa7a
mqois16 <qoisoctava@gmail.com>
2021-11-26 14:25:31 +0700

    Timer done

---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    public static TimerController instance;
+
+    public Text timeCounter;
+
+    private TimeSpan timePlaying;
+    private bool timerGoing;
+
+    private float elapsedTime;
+
+    private Transform player;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        timeCounter.text = "00:00.00";
+        timerGoing = false;
+        player = GameObject.FindWithTag("Player").transform;
+
+        if (player)
+        {
+            BeginTimer();
+        }
+        
+    }
+
+    private void Update()
+    {
+        if (player == null)
+        {
+            EndTimer();
+            string timePlayingStr = timePlaying.ToString("mm':'ss'.'ff") + "\nGame Over";
+            timeCounter.text = timePlayingStr;
+        }
+    }
+
+    public void BeginTimer()
+    {
+        timerGoing = true;
+        elapsedTime = 0f;
+
+        StartCoroutine(UpdateTimer());
+    }
+
+    public void EndTimer()
+    {
+        timerGoing = false;
+    }
+
+    private IEnumerator UpdateTimer()
+    {
+        while (timerGoing)
+        {
+            elapsedTime += Time.deltaTime;
+            timePlaying = TimeSpan.FromSeconds(elapsedTime);
+            string timePlayingStr = timePlaying.ToString("mm':'ss'.'ff");
+            timeCounter.text = timePlayingStr;
+
+            yield return null;
+        }
+    }
+
+   
+}
